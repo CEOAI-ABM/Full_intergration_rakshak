fd1d8d309681661ba8c0bcf349bcb5a90538d77e
Aakarsh2000 <58129140+Aakarsh2000@users.noreply.github.com>
2020-12-28 17:03:38 +0530

    Update academic.py

--- src/academic.py
+++ src/academic.py
@@ -68,3 +68,56 @@ class Academic:
 #    a = Academic([[2,2,1],[20,10,5],[[1,2],[3,4],[5]],[[1,2],[3,4],[5]],[[1,2],[3,4],[5]],[[10,20],[30,40],[5]]])
 #    print(a.Daily_People_Expectation)
 #    print(a.Units_Placeholder)
+import numpy as np
+
+class Residence_Unit:
+    """A Class for storing a unit of a Residence i.e., a room of a hostel or a house or mess
+        Takes 5 parameters
+        Id no. for an residence i.e., each residence is given a Id No.
+        Room No. of place
+        Height and Location
+    """
+    def __init__(self,Id,Building,Daily_People_Expectation,Number_Workers,Height,x_coordinate,y_coordinate,Inter_Person_Distance):
+
+        self.Id                         = Id
+        self.Building                   = Building
+        self.Residence                  = Residence
+        self.Daily_People_Expectation   = Daily_People_Expectation
+        self.Number_Workers             = Number_Workers
+        self.height                     = Height
+        self.x                          = x_coordinate
+        self.y                          = y_coordinate
+        self.Inter_Person_Distance      = Inter_Person_Distance
+        self.working                    = []
+        self.visiting                   = []
+
+class Residence:
+    """Class for storing
+    Args:
+        0th pm is number of rooms
+        1st pm is the number of workers
+        2nd pm is the x-coordinates
+        3rd pm is the y-coordinates
+        4th pm the heights
+        5th pm the hostel name
+    """
+    def __init__(self,pm):
+
+        self.Number_Rooms               = pm[0]
+        self.Total_Num_Buildings        = len(self.Number_Rooms)
+        self.Number_Workers             = pm[1]
+        self.height                     = pm[2]
+        self.Location                   = [pm[3],pm[4]]
+        self.Daily_People_Expectation   = pm[5]
+        self.Name                       = pm[6]
+        self.Inter_Person_Distance      = pm[7]
+
+        self.Units_Placeholder={}
+
+
+    def initialize_units(self):
+        for i in range(len(self.Number_Rooms)):
+            if(self.Number_Rooms[i]>0):
+                self.Units_Placeholder[Name[i]]=[];
+                for j in range(self.Number_Rooms[i]):
+                    self.Units_Placeholder[].append(Residence_Unit(j,i,self.Daily_People_Expectation[i][j],self.Number_Workers[i][j],self.Height[i][j],self.Location[0][i][j],self.Location[1][i][j],self.Inter_Person_Distance[i][j]))
