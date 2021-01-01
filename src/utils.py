36a0682fbb84bcce8a904af93f11441e511228f1
thefatbandit <arnesh.issar@gmail.com>
2021-01-02 05:12:21 +0530

    add student init (limited to 2nd-5th year UG of engineering dept)

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
 import json
+import random
 
 def total_students(course, grades, grades_18A, grades_18S, na_list):
     """Gives the count of total students present in the course 
@@ -23,7 +24,7 @@ def total_students(course, grades, grades_18A, grades_18S, na_list):
             print(course_grades[grade])
             strength = strength + course_grades[grade]
 
-        return (strength, course)
+        return strength
 
     except KeyError:
         if course in grades_18A:
@@ -34,7 +35,7 @@ def total_students(course, grades, grades_18A, grades_18S, na_list):
                 print(course_grades[grade])
                 strength = strength + course_grades[grade]
 
-            return (strength, course)
+            return strength
         elif course in grades_18S:
             course_grades = grades_18S[course]["grades"]
 
@@ -43,7 +44,7 @@ def total_students(course, grades, grades_18A, grades_18S, na_list):
                 print(course_grades[grade])
                 strength = strength + course_grades[grade]
 
-            return (strength,course)
+            return strength
         else:
             try:
                 txt = course + "- " + grades[course]["name"]
@@ -54,7 +55,7 @@ def total_students(course, grades, grades_18A, grades_18S, na_list):
                     na_list.append(course)
 
             print(course + " grades not found")
-            return ("NA",course)
+            return "NA"
 
 
 def gen_timetable(file_path, schedule, grades, grades_18A, grades_18S):
@@ -87,7 +88,7 @@ def gen_timetable(file_path, schedule, grades, grades_18A, grades_18S):
                 course  = day[j]
                 if course != "":
                     print(course + " initiated")
-                    strengths[timmings[j]] = total_students(course, grades, grades_18A, grades_18S, na_list)
+                    strengths[timmings[j]] = (total_students(course, grades, grades_18A, grades_18S, na_list), course)
                 else:
                     strengths[timmings[j]] = 0
 
@@ -101,6 +102,113 @@ def gen_timetable(file_path, schedule, grades, grades_18A, grades_18S):
         print(entry)
 
     return na_list
+
+def form_subjects_real(file_path, subjects, grades, grades_18A, grades_18S):
+    na_list = []
+    final_subjects = {}
+
+    for subject in subjects:
+        temp = {}
+        temp["strength"] = total_students(subject, grades, grades_18A, grades_18S, na_list)
+        temp["slots"] = subjects[subject][0]
+        temp['room'] = subjects[subject][1]
+        final_subjects[subject] = temp
+    
+    with open(file_path + "/subjects.json",'w') as fp:
+        json.dump(final_subjects, fp, sort_keys=True, indent=3)
+    
+    return na_list
+
+def is_valid(subject):
+    """ Checks whether the subject has sufficient information to be added to the 
+    """
+    if(subject["strength"]=="NA"):
+        return False
+    
+    return True
+
+def is_clash(slot,lab_slots):
+    """ Checks for clash of slot with 
+    """
+    clash   = {'Q-Lab' : ['C3','B3','D3','D4'], "J-Lab" : ['H3','U3','U4'], "K-Lab" : ['D2','D3','D3','A3'], "L-Lab" : ['H2','H3','U3','U4'], "R-Lab" : ['F3','F4','G3','E3','E4'], "X-Lab" : ['X4'], "M-Lab" : ['C4','E3','E4','G3'], "N-Lab" : ['I2','V3','V3','V4'], "O-Lab" : ['E2','E4','F4','F3'], "P-Lab" : ['V4','V3','I2']}
+
+    for entry in lab_slots:
+        if(slot in clash[entry]):
+            return True
+    
+    return False
+
+def form_schedule(file_path=None,save=False):
+    """ For generating shcedule for various departments
+
+    Args:
+        file_path(str): For storing the schedule.json
+        save(bool): Whether to store the schedule  
+    """
+
+    depts   = ['AE', 'AG', 'AI', 'BT', 'CE', 'CH', 'CS', 'CY', 'EC', 'EE', 'EX', 'GG', 'HS', 'IE', 'IM', 'MA', 'ME', 'MF', 'MI', 'MT', 'NA', 'PH', 'QE', 'QM', 'RR']
+    rooms   = ['NC131', 'NC132', 'NC141', 'NC142', 'NC231', 'NC232', 'NC233', 'NC234', 'NC241', 'NC242', 'NC243', 'NC244', 'NC331', 'NC332', 'NC333', 'NC334', 'NC341', 'NC342', 'NC343', 'NC344', 'NC431', 'NC432', 'NC433', 'NC434', 'NC441', 'NC442', 'NC443', 'NC444', 'NEX201', 'NR121', 'NR122', 'NR123', 'NR124', 'NR221', 'NR222', 'NR223', 'NR224', 'NR321', 'NR322', 'NR323', 'NR324', 'NR421', 'NR422', 'NR423', 'S-123', 'S-125', 'S-126', 'S-127', 'S-136', 'S-216', 'S-122A', 'S-301', 'S-302', 'V1', 'V2', 'V3', 'V4']
+    slots_class   = ['A3', 'B3', 'C3', 'C4', 'D3', 'D4', 'E3', 'E4', 'F3', 'F4', 'G3', 'H3', 'S3', 'U3', 'U4', 'V3', 'V4', 'X4']
+    slots_lab = ['J-Lab', 'K-Lab', 'L-Lab', 'M-Lab', 'N-Lab', 'O-Lab', 'P-Lab', 'Q-Lab', 'R-Lab', 'X-Lab']
+    
+
+    occupied = []
+    schedule = {}
+
+    for dept in depts:
+
+        for i in range(2,5):
+            temp_dict = {}
+            year_schedule = {}
+            lab_slot_taken = []
+            occupied_slots = []
+            for j in range(1,random.randrange(3,5)):
+                room    = dept + "-" + str(i) +  "-Lab-" + str(j)
+                slot    = random.choice(slots_lab)
+
+                while(slot in lab_slot_taken):
+                    room    = random.choice(rooms)
+                    slot    = random.choice(slots_lab)
+                else:
+                    occupied_slots.append(slot)
+                    lab_slot_taken.append(slot)
+                    year_schedule[room] = {"slot" : slot, "room" : room}
+
+            for j in range(1,random.randrange(4,6)):
+                room    = random.choice(rooms)
+                slot    = random.choice(slots_class)
+
+                ctr = 0
+                while((slot in occupied_slots) and ((room + "_" + slot) in occupied) and is_clash(slot,lab_slot_taken) and ctr<50):
+                    room    = random.choice(rooms)
+                    slot    = random.choice(slots_class)
+                    ctr = ctr+1
+                else:
+                    if(ctr>50):
+                        while((slot in occupied_slots) or is_clash(slot,lab_slot_taken)):
+                            slot = random.choice(slots_class)
+
+                        else:
+                            room = dept + "-room-" + str(j)
+                            occupied.append(room + "_" + slot)
+                            year_schedule[dept + "-" + str(i) + "-" + str(j)] = {"slot" : slot, "room" : room}
+                    else:
+                        occupied.append(room + "_" + slot)
+                        occupied_slots.append(slot)
+                        year_schedule[dept + "-" + str(j)] = {"slot" : slot, "room" : room}
+                        
+                
+            
+            if(dept in schedule):
+                schedule[dept].update({i:year_schedule})
+            
+            else:
+                temp_dict[i] = year_schedule
+                schedule[dept] = temp_dict
+            
+    return schedule
+
+
 
 def main():
     # Can be set to wherever the json files are present
@@ -118,11 +226,21 @@ def main():
     with open(file_path + "/Grades/2018Spring.json", 'r') as fp:
         grades_18S = json.load(fp)
 
-    na_list = gen_timetable(file_path, schedule, grades, grades_18A, grades_18S)
+    with open(file_path + "/subjects.json",'r') as fp:
+        subjects = json.load(fp)
 
-    with open(file_path + "/na_subjects_list.txt",'w') as fp:
-        for entry in na_list:
-            fp.write(entry + "\n")
+    with open(file_path + "/Schedule/slots.json",'r') as fp:
+        slots = json.load(fp)
+
+    schedule = form_schedule(file_path)
+    print(schedule)
+
+    # na_list = form_subjects_real(file_path, subjects, grades, grades_18A, grades_18S)
+    # na_list = gen_timetable(file_path, schedule, grades, grades_18A, grades_18S)
+    
+    # with open(file_path + "/na_subjects_list.txt",'w') as fp:
+    #     for entry in na_list:
+    #         fp.write(entry + "\n")
 
 
 if __name__ == "__main__":
