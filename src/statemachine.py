8dc5ffe64cdff9fecc8a147451977ca94d2519c0
KartheekVajrala <48596971+KartheekVajrala@users.noreply.github.com>
2021-01-06 14:27:31 +0530

    Update and rename agentStatusA.py to statemachine.py
    Added Testing State

--- src/statemachine.py
+++ src/statemachine.py
@@ -1,3 +1,5 @@
+import transitions
+
 class AgentStatusA():
 	"""docstring for AgentStatus"""
 	status  = ['Free','Quarentined','Out_of_campus','Hospitalized','ICU','Isolation']
@@ -103,3 +105,49 @@ class AgentStateA():
 		return self.State == self.states[3]
 	def is_Died(self):
 		return self.State == self.states[4]
+	
+class TestingState():
+    """
+    TestingQueue : List of people currently in testing State
+    TestedP : Dictionary of people based on test result
+    """
+    TestingQueue = []
+    TestedP = {'Positive': [], 'Negative': []}
+
+    
+    machine = transitions.Machine(model=None, states=['Not_tested', 'Awaiting_Testing', 'Tested_Positive','Tested_Negative'], initial='Not_tested',
+                      transitions=[
+                          {'trigger': 'awaiting_test', 'source': ['Not_tested','Awaiting_Testing','Tested_Negative'], 'dest': 'Awaiting_Testing','before':'add_to_TestingQueue'},
+                          {'trigger': 'tested_positive', 'source': 'Awaiting_Testing', 'dest': 'Tested_Positive','before':'tested_positive_func'},
+                          {'trigger': 'tested_negative', 'source': 'Awaiting_Testing', 'dest': 'Tested_Negative','before':'tested_negative_func'},
+                      ])
+    def __init__(self): 
+        
+        self.state         = 'Not_tested'
+
+    def add_to_TestingQueue(self, PrivateTest=False): 
+        
+        if PrivateTest == False:
+            if self.state == 'Not_tested':
+                TestingState.TestingQueue.append(self)
+
+    def tested_positive_func(self, PrivateTest=False): 
+        
+        TestingState.TestedP['Positive'].append(self)
+
+        if PrivateTest == False: 
+            self.__remove_from_testing_list__()
+        
+        if self.is_Quarentined():
+            self.isolate()
+
+
+    def tested_negative_func(self, PrivateTest=False):
+        TestingState.TestedP['Negative'].append(self)
+
+        if PrivateTest == False:
+            self.__remove_from_testing_list__()
+
+    def __remove_from_testing_list__(self): 
+        TestingState.TestingQueue.remove(self)
+
