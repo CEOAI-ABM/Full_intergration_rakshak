4358d800ef8b7509cb9b7de2509d6e12dec89237
Vikram <sbsana57@gmail.com>
2021-01-29 17:18:01 +0530

    Updated viz.py

--- src/viz.py
+++ src/viz.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import numpy as np
 
 from utils import form_schedule
 from param_with_shp import Parameters
@@ -21,6 +22,8 @@ def main():
         pointa = []
         pointb = []
         for key, value in p[t].get_timetable().items():
+            if key not in ['monday'] :
+                continue
             for aa, b in value.items():
                 pointa.append(b.x)
                 pointb.append(b.y)
@@ -47,18 +50,42 @@ def main():
     mat, = ax.plot(x, y, '.', markersize=1)
 
     def init():
-        mat.set_data([], [])
+        mat.set_data([pa[k][0] for k in range(len(p))], [pb[k][0] for k in range(len(p))])
         for i in a.ParamObj.polygons:
             ax.plot(*i.exterior.xy, lw=1)
+        return mat,
+
+    xarray = []
+    yarray = []
+    numframe = 30
+    totframes = 0
+    for i in range(numframe*25):
+        try:
+            if i % numframe == 0:
+                j = i//numframe
+                x = [pa[k][j] for k in range(len(p))]
+                y = [pb[k][j] for k in range(len(p))]
+            else:
+                m = i%numframe
+                n = numframe-m
+                x = [(n*pa[k][i//numframe]+m*pa[k][(i//numframe)+1])/numframe+np.random.normal(0,0.2,1) for k in range(len(p))]
+                y = [(n*pb[k][i//numframe]+m*pb[k][(i//numframe)+1])/numframe+np.random.normal(0,0.2,1) for k in range(len(p))]
+            xarray.append(x)
+            yarray.append(y)
+            totframes+=1
+        except:
+            break
 
     # animation function.  This is called sequentially
     def animate(i):
-        x = [pa[k][i] for k in range(len(p))]
-        y = [pb[k][i] for k in range(len(p))]
+        x = xarray[i]
+        y = yarray[i]
         mat.set_data(x, y)
         return mat,
 
-    anim = animation.FuncAnimation(fig, animate, init_func=init, interval=600)
+    anim = animation.FuncAnimation(fig, animate, init_func=init, interval=20, blit=True, repeat=False, save_count=3*totframes//4)
+#    writergif = animation.PillowWriter(fps=50)
+#    anim.save('student.gif',writer=writergif)
     plt.show()
 
 
