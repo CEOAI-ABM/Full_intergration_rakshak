2f10f36ba14b74f5934b4c3871811033b1f9ad1b
Vikram <sbsana57@gmail.com>
2021-02-05 13:17:13 +0530

    Added profs movements

--- src/viz.py
+++ src/viz.py
@@ -6,6 +6,7 @@ from utils import form_schedule
 from param_with_shp import Parameters
 from sector import Sector
 from person import __init_students__
+from Non_Student import init_profs_from_schedule
 
 
 def main():
@@ -13,7 +14,9 @@ def main():
     pm = Parameters('shapes/KgpBuildings.shp', 'Campus_data/KGP Data - Sheet1.csv')
     a = Sector(pm.returnParam())
     p = __init_students__(schedule, a)
+    profs = init_profs_from_schedule(schedule,a)
     print(p[0].get_timetable())
+    print(profs[1].daily_schedule_expected)
 
     pa = []
     pb = []
@@ -41,6 +44,18 @@ def main():
     pa.append(pointa)
     pb.append(pointb)"""
 
+    for t in range(len(profs)):
+        pointa = []
+        pointb = []
+        for key, value in profs[t].daily_schedule_expected.items():
+            if key not in ['monday'] :
+                continue
+            for aa, b in value.items():
+                pointa.append(b.x)
+                pointb.append(b.y)
+        pa.append(pointa)
+        pb.append(pointb)
+
 #    print(pointa,pointb)
     fig = plt.figure()
     for i in a.ParamObj.polygons:
@@ -50,26 +65,26 @@ def main():
     mat, = ax.plot(x, y, '.', markersize=1)
 
     def init():
-        mat.set_data([pa[k][0] for k in range(len(p))], [pb[k][0] for k in range(len(p))])
+        mat.set_data([pa[k][0] for k in range(len(p)+len(profs))], [pb[k][0] for k in range(len(p)+len(profs))])
         for i in a.ParamObj.polygons:
             ax.plot(*i.exterior.xy, lw=1)
         return mat,
 
     xarray = []
     yarray = []
-    numframe = 30
+    numframe = 20
     totframes = 0
     for i in range(numframe*25):
         try:
             if i % numframe == 0:
                 j = i//numframe
-                x = [pa[k][j] for k in range(len(p))]
-                y = [pb[k][j] for k in range(len(p))]
+                x = [pa[k][j] for k in range(len(p)+len(profs))]
+                y = [pb[k][j] for k in range(len(p)+len(profs))]
             else:
                 m = i%numframe
                 n = numframe-m
-                x = [(n*pa[k][i//numframe]+m*pa[k][(i//numframe)+1])/numframe+np.random.normal(0,0.2,1) for k in range(len(p))]
-                y = [(n*pb[k][i//numframe]+m*pb[k][(i//numframe)+1])/numframe+np.random.normal(0,0.2,1) for k in range(len(p))]
+                x = [(n*pa[k][i//numframe]+m*pa[k][(i//numframe)+1])/numframe+np.random.normal(0,0.2,1) for k in range(len(p)+len(profs))]
+                y = [(n*pb[k][i//numframe]+m*pb[k][(i//numframe)+1])/numframe+np.random.normal(0,0.2,1) for k in range(len(p)+len(profs))]
             xarray.append(x)
             yarray.append(y)
             totframes+=1
@@ -83,9 +98,9 @@ def main():
         mat.set_data(x, y)
         return mat,
 
-    anim = animation.FuncAnimation(fig, animate, init_func=init, interval=20, blit=True, repeat=False, save_count=3*totframes//4)
-#    writergif = animation.PillowWriter(fps=50)
-#    anim.save('student.gif',writer=writergif)
+    anim = animation.FuncAnimation(fig, animate, init_func=init, interval=20, blit=True, repeat=False, save_count=totframes)
+#    writergif = animation.PillowWriter(fps=40)
+#    anim.save('student_and_prof.gif',writer=writergif)
     plt.show()
 
 
