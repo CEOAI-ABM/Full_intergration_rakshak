401d480c82149dd08c2215490127506af6062644
Vikram <sbsana57@gmail.com>
2021-02-26 14:37:20 +0530

    Updated code to give all buildings single floors

--- src/contact_graph.py
+++ src/contact_graph.py
@@ -145,16 +145,10 @@ def getContacts(deviceid, time_ref):
 	g.show("nx.html")
 	
 	return node_list, edges_list
-	
-	#####Visualize###############
-#	value_list[0]= max(value_list)*2
-#	g= Network("500px", "500px",directed=False)
-#	g.add_nodes(node_list,label=label_list,title=title_list,value=value_list,color=color_list)
-#	g.add_edges(edges_list)
-#	g.show("nx.html")
+
 
 def fullgraphplot(time_lower,time_upper):
-    """ 
+	"""
     Function print the overall node graph within the selected time window. 
   
     Require two table named identity and activity to retrive the data. 
@@ -168,16 +162,16 @@ def fullgraphplot(time_lower,time_upper):
   
     """
 
-    edges_list,node_list,title_list = graphformation(time_lower,time_upper)
-    node_size = []
-    for i in range(len(node_list)):
-        node_size.append(5)
-    g = Network(
+	edges_list,node_list,title_list = graphformation(time_lower,time_upper)
+	node_size = []
+	for i in range(len(node_list)):
+		node_size.append(5)
+		g = Network(
             height="750px",
             width="100%",
             bgcolor="#222222",
             font_color="white")
-    g.add_nodes(node_list,label=node_list,title=title_list, size= node_size)
-    g.add_edges(edges_list)
-    g.show("nx.html")
-    return
+	g.add_nodes(node_list,label=node_list,title=title_list, size= node_size)
+	g.add_edges(edges_list)
+	g.show("nx.html")
+	return
